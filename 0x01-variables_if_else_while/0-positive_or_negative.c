d647d07f46b3cb121c7db1d1101a5e3859e21f27
Okameh <okworiemmaa@gmail.com>
2022-12-09 04:34:53 -0800

    first variable

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints a negative or a positive number or 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	strand(time(0));
+	n = rand() - Rand_Max / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+c variables and conditional statements
