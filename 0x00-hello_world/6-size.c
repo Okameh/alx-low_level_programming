b6296ef6a0aa74eb1b609a4f4035ebb559a542f6
Okameh <okworiemmaa@gmail.com>
2022-12-08 22:03:37 -0800

    good

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  main - my next code
+ *  Return: Always 0
+ */
+int main(void)
+{
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("size of long long int: %i byte(s)\n", sizeof(long long));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
