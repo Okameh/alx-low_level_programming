d9b215001108c2eff577c23dc546274cd3ccd7b4
Okameh <okworiemmaa@gmail.com>
2022-12-08 21:08:37 -0800

    yeep

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include stdio.h
+#include <stdio.h>
 
 /**
  *  main - Entry
@@ -7,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"):
-		return (0):
+	return (0):
 }
