26b5eaebfede3bdcd7da3d205a4ce8809c016e7e
Okameh <okworiemmaa@gmail.com>
2023-01-07 08:21:58 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * __putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+ int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
