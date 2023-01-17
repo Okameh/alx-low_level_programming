a9eb3b3ff69ec6e8779380861bfdf16d5869f4a9
Okameh <okworiemmaa@gmail.com>
2023-01-16 16:10:11 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+ * free_dog - frees money allocated for a struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
